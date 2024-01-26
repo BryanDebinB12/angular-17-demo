fa553812238faf1a299146bdb98e325eddb4bcfb
Brieuc Kaisin <brieuc.kaisin@b12-consuting.com>
2024-01-26 10:24:43 +0100

    typed MyVeryOwnTableComponent input data

--- src/app/ng-template-outlet-example/my-very-own-table/my-very-own-table.component.ts
+++ src/app/ng-template-outlet-example/my-very-own-table/my-very-own-table.component.ts
@@ -8,8 +8,8 @@ import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
   templateUrl: './my-very-own-table.component.html',
   styleUrl: './my-very-own-table.component.scss',
 })
-export class MyVeryOwnTableComponent {
-  @Input() data!: any[];
-  @ContentChild('headers') headers: TemplateRef<any> | undefined;
-  @ContentChild('rows') rows: TemplateRef<any> | undefined;
+export class MyVeryOwnTableComponent<T> {
+  @Input() data!: T[];
+  @ContentChild('headers') headers: TemplateRef<unknown> | undefined;
+  @ContentChild('rows') rows: TemplateRef<unknown> | undefined;
 }
